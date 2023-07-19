26ab75663301956bbb64ad3f55e4eb5b1f9064eb
jesusvite <nicolasvitejesus@gmail.com>
2023-07-19 11:19:59 -0600

    agregue codigo para borrar los items y para marcar como hechos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,14 +3,19 @@ import './TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>
+      <span className={
+        `Icon Icon-check 
+        ${props.completed && 
+          "Icon-check--active"}`}
+          onClick={props.onComplete}
+        >
         o
       </span>
       {/* <correcto value={props.completed} /> */}
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">
+      <span className="Icon Icon-delete"     onClick={props.onDelete}>
         X
       </span>
     </li>
